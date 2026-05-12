995fa7d72d0d0a62fcac5af68cc1c0f3e7058e5f
rajaagl <rajaa.mahroug@uit.ac.ma>
2026-05-12 09:54:44 +0100

    ensure base path is correctly set in Vite configuration

--- ramaqs-management-app/vite.config.ts
+++ ramaqs-management-app/vite.config.ts
@@ -14,6 +14,7 @@ const config = defineConfig({
     tanstackRouter({ target: 'react', autoCodeSplitting: true }),
     viteReact(),
   ],
+   base: '/ramaqs-management-app/',
 })
 
 export default config
